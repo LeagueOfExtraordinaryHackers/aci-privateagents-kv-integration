15397df944f0d0d756b0113e4d5734cf7c142481
Alessandro Vozza <alessandro.vozza@microsoft.com>
2020-05-04 13:45:30 +0200

    path

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module aci-devops-agents {
 
-  source              = "modules"
+  source              = "./modules"
   resource_group_name = var.resource_group_name
   location            = var.location
   agent_version       = var.agent_version
