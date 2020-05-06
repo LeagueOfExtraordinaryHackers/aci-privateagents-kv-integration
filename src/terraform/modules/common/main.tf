741957e9e4ee9594da9a5dbcdd0b9d27cf2c55b3
Alessandro Vozza <alessandro.vozza@microsoft.com>
2020-05-06 13:12:36 +0200

    remove accss policy

--- src/terraform/modules/common/main.tf
+++ src/terraform/modules/common/main.tf
@@ -92,7 +92,7 @@ resource "azurerm_key_vault_secret" "sshkey" {
 }
 
 #remove permissions to current SP
-/* 
+
 resource "azurerm_key_vault_access_policy" "remove_current" {
   key_vault_id = azurerm_key_vault.keyvault.id
 
@@ -112,9 +112,9 @@ resource "azurerm_key_vault_access_policy" "remove_current" {
   ]
 
   depends_on = [
-    azurerm_key_vault_access_policy.current,
+    azurerm_key_vault_secret.sshkey,
   ]
-} */
+}
 
 #add secret read access to the MI
 resource "azurerm_key_vault_access_policy" "mi" {
