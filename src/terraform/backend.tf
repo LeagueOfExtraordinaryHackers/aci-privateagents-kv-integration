effdc5e6c9a17acf8a239173c8fbf352e48397ca
Alessandro Vozza <alessandro.vozza@microsoft.com>
2020-05-06 09:09:23 +0200

    fixed suffix and backed

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -1,1 +1,4 @@
-backend = "azurerm"
+terraform {
+  backend "azurerm" {
+  }
+}
--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -2,12 +2,12 @@ module "common" {
   source              = "./modules/common"
   resource_group_name = var.resource_group_name
   location            = var.location
-  suffix              = random_string.suffix.result
+  suffix              = var.suffix
 }
 
 module "aci-devops-agents" {
   source              = "./modules/aci-devops-agents"
   resource_group_name = var.resource_group_name
   location            = var.location
-  suffix              = random_string.suffix.result
+  suffix              = var.suffix
 }
