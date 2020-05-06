ff2af3f69765105fd35e63c3f08208e9128f5b8c
Alessandro Vozza <alessandro.vozza@microsoft.com>
2020-05-06 09:26:39 +0200

    vnet

--- src/terraform/modules/common/main.tf
+++ src/terraform/modules/common/main.tf
@@ -2,3 +2,15 @@ resource azurerm_resource_group "rg" {
   name     = "${var.resource_group_name}-${var.suffix}"
   location = var.location
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${var.suffix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+}
