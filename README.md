e48e8fbad4a69585600887198688d846f8d7d99f
Alessandro Vozza <alessandro.vozza@microsoft.com>
2020-05-06 09:46:05 +0200

    readme and kv

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 
 
 This repo shows how to build a scalable pool of Azure DevOps ACI-based agents that have private access to a secured Keyvault to retrieve secrets (in this case, an SSH private key) to operate over air-gapped resources in Azure. 
+
+It deploys:
+
+- A common module to build the basic infrastructure:
+  - A resource group
+  - A vnet and a subnet
+  - A managed identity
+  - A Keyvault
--- src/terraform/modules/common/main.tf
+++ src/terraform/modules/common/main.tf
@@ -14,3 +14,49 @@ resource "azurerm_virtual_network" "vnet" {
     address_prefix = "10.0.1.0/24"
   }
 }
+
+resource "azurerm_user_assigned_identity" "mi" {
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = "${azurerm_resource_group.rg.location}"
+
+  name = "kvaccess"
+}
+
+#necessary for Keyvault
+data "azurerm_client_config" "current" {}
+
+
+resource "azurerm_key_vault" "key_vault" {
+  name                        = "kv-${var.suffix}"
+  resource_group_name         = "${azurerm_resource_group.rg.name}"
+  location                    = "${azurerm_resource_group.rg.location}"
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+
+  network_acls {
+    default_action             = "Deny"
+    bypass                     = "AzureServices"
+    virtual_network_subnet_ids = ["${azurerm_virtual_network.vnet.subnet.id}"]
+  }
+
+  # authorize the current principal to access the keyvault secrets, keys and storage
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = azurerm_user_assigned_identity.mi.id
+
+    key_permissions = [
+    ]
+
+    secret_permissions = [
+      "get",
+      "list"
+    ]
+
+    storage_permissions = [
+    ]
+
+    certificate_permissions = [
+    ]
+  }
+}
